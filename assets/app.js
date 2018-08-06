0601ffab1a91e85d28b0a102ce21bd59218512ca
james inch <james.inch0103@gmail.com>
2018-08-06 18:33:43 -0400

    minor changes

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,6 @@
-/* 
-I need and array to hold all my initial buttons 
-loop thru the array
-*/
-
 $(document).ready(function () {
 
-    var gifs = ["Skateboarding", "Surfing", "Snowboarding", "Battlefeild 3", "Battlefeild 4", " Battlefeild V", "Fortnite", "Rock Climbing", "Dogs", "Cats", "Boats", "Wake surfing", "Rick and Morty", "Motorcycles"];
+    var gifs = ["Skateboarding", "Surfing", "Snowboarding", "Battlefield 3", "Battlefield 4", "GTA V", "PUBG", "Fortnite", "Rock Climbing", "Dogs", "Cats", "Boats", "Wake surfing", "Rick and Morty", "Motorcycles"];
 
     function showGifs() {
 
@@ -73,14 +68,4 @@ $(document).ready(function () {
     renderButtons();
 
 });
-// When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-// When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
 
-// Under every gif, display its rating (PG, G, so on).
-
-// This data is provided by the GIPHY API.
-// Only once you get images displaying with button presses should you move on to the next step.
-
-// Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page.
-// Deploy your assignment to Github Pages.
-// Rejoice! You just made something really cool.
